e479508e78bc212fc69ddce7c573189dbdd81f84
izzio <izzidin.oakes@antechdx.com>
2026-09-19 12:14:46 -0400

    add: git diff and lua lint

--- trotly/lazy.lua
+++ trotly/lazy.lua
@@ -7,8 +7,34 @@ return {
       vim.cmd.colorscheme "catppuccin-mocha"
     end,
   },
-   {
-    "sphamba/smear-cursor.nvim",
-    opts = {},
+
+ {
+  "sphamba/smear-cursor.nvim",
+  opts = {},
+  },
+
+ {
+  "f-person/git-blame.nvim",
+  event = "VeryLazy",
+  opts = {
+    enabled = true},
+  },
+
+  {
+    "mfussenegger/nvim-lint",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      require("lint").linters_by_ft = {
+        lua = { "luacheck" },
+      }
+
+      vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+        callback = function()
+          require("lint").try_lint()
+        end,
+      })
+    end,
   }
+
 }
+
